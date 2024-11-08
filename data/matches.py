f487fc8ef9e8435074a9be8957fce11c3e7ae006
Dasevil <88722406+Dasevil@users.noreply.github.com>
2024-11-08 08:15:12 +0300

    Create matches.py

---
+++ data/matches.py
@@ -0,0 +1,6 @@
+class Matches:
+    def __init__(self, id: int, users_ids: [int], timeStart: float, timeEnd: float):
+        self.id = id
+        self.users_ids = users_ids
+        self.time_start = timeStart
+        self.time_end = timeEnd
